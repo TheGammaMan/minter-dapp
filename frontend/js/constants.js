cec55370336af40f9d13a5ac8ddd90d02f1f122d
TheGammaMan <gammapunksnft@gmail.com>
2022-04-11 03:47:27 +0530

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x7d34018798fBEce4175c8f7d43943C0046cCA3e4"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
